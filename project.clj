0e2e6697c151c541521e790b7151c32c210fd769
Aryabhatiya Algebra <aryabhatiya.algebra@gmail.com>
2017-08-09 18:05:57 +0600

    added my cv

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[com.cemerick/pomegranate "0.3.1"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.671" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.300"]
                  [ring "1.6.2"]
